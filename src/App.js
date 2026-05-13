44da465ee5deea0b1c1df0d987b69449d68375e4
hzmsalah <hzmsalah2@gmail.com>
2026-05-13 20:51:37 +0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import {use, useRef, useState} from 'react';
 
 function App() {
+  const inputRef = useRef(null);
+  // result ref is for the display in the result section
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  function plus(e){
+    e.preventDefault();
+    const inputVal = Number(inputRef.current.value);
+    const newResult = result + inputVal;
+    setResult(newResult);
+  }
+
+  function minus(e){
+  e.preventDefault();
+  const inputVal = Number(inputRef.current.value);
+  const newResult = result - inputVal;
+  setResult(newResult);
+  }
+
+  function multiply(e){
+  e.preventDefault();
+  const inputVal = Number(inputRef.current.value);
+  const newResult = result * inputVal;
+  setResult(newResult);
+  }
+
+  function divide(e){
+  e.preventDefault();
+  const inputVal = Number(inputRef.current.value);
+  if(inputVal === 0){
+    alert("Cannon Divide By 0!!")
+    return
+  }
+  const newResult = result / inputVal;
+  setResult(newResult);
+  }
+
+
+  function resetInput(e){
+    e.preventDefault();
+    inputRef.current.value = "";
+  }
+
+  function resetResult(e){
+    e.preventDefault();
+    setResult(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h1>
+                  Simple Calculator App
+        </h1>
+        <p ref={resultRef}>
+          {result}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <form>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>Add</button>
+        <button onClick={minus}>Subtract</button>
+        <button onClick={multiply}>Multiply</button>
+        <button onClick={divide}>Divide</button>
+        <button onClick={resetInput}>Reset Input</button>
+        <button onClick={resetResult}>Reset Result</button>
+      </form>
     </div>
   );
 }
